0a5254f0abc636e69dbe84076eb6caa4d36377f6
Sally-N <snzungula@gmail.com>
2021-01-05 17:28:56 +0300

    bhghj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import Todos from "./components/Todos";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-       Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+class App extends Component {
+  state = {
+      Todos: [
+          {
+              id: '1',
+              title: 'Take out the trash',
+              completed: true
+          },
+          {
+              id: '2',
+              title: 'Make bed',
+              completed: false
+          },
+          {
+              id: '3',
+              title: 'Wash dishes',
+              completed: false
+          },
+      ]   
+  }    
+
+  markComplete = () => {
+    console.log('From app.js')
+}                                                                         
+  render() {
+      return (
+          <div className="App">
+          <Todos todos={this.state.Todos} markComplete={this.markComplete} />
+          </div>
+      );
+      }
+  }
 export default App;
+
